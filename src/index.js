ac40be6ce673e8db56aa9fd3f405e3f72daccc65
petra <petra.spirkova@apiumhub.com>
2019-06-04 12:29:03 +0200

    use props.children

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const App  = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail
                     author="Sam"
                     timeAgo="Today at 4:45PM"
@@ -15,18 +21,22 @@ const App  = () => {
                     avatar={faker.image.avatar()}
                 />
             </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail
                 author="Alex"
                 timeAgo="Today at 2:00PM"
                 content="Thanks!"
                 avatar={faker.image.avatar()}
             />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail
                 author ="Jane"
                 timeAgo="Yesterday at 5:00PM"
                 content="Cool post!"
                 avatar={faker.image.avatar()}
             />
+            </ApprovalCard>
         </div>
     )
 };
